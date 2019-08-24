172a5331297978c18b51aa54062304c110b05c09
Matthew Adams <matthew.adams@endjin.com>
2019-08-25 00:07:03 +0100

    Added additional converter for ContentEnvelope to ensure it will deserialize correctly.

---
+++ Solutions/Corvus.ContentHandling.Json/Corvus/ContentHandling/Json/Internal/ContentEnvelopeConverter.cs
@@ -0,0 +1,59 @@
+﻿// <copyright file="ContentEnvelopeConverter.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Corvus.ContentHandling.Json.Internal
+{
+    using System;
+    using System.Threading;
+    using Corvus.Extensions.Json;
+    using Microsoft.Extensions.DependencyInjection;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// A standard json converter for <see cref="ContentEnvelope"/>.
+    /// </summary>
+    public class ContentEnvelopeConverter : JsonConverter
+    {
+        private const string SerializedPayloadTag = "payload";
+        private const string PayloadContentTypeTag = "contentType";
+        private readonly IServiceProvider serviceProvider;
+        private readonly Lazy<IJsonSerializerSettingsProvider> jsonSerializerSettings;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContentEnvelopeConverter"/> class.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider for the context.</param>
+        public ContentEnvelopeConverter(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+            this.jsonSerializerSettings = new Lazy<IJsonSerializerSettingsProvider>(() => this.serviceProvider.GetService<IJsonSerializerSettingsProvider>(), LazyThreadSafetyMode.ExecutionAndPublication);
+        }
+
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(ContentEnvelope) == objectType;
+        }
+
+        /// <inheritdoc/>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var value = JToken.ReadFrom(reader) as JObject;
+            return ContentEnvelope.FromJson(value[SerializedPayloadTag], (string)value[PayloadContentTypeTag], this.jsonSerializerSettings.Value?.Instance);
+        }
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var env = (ContentEnvelope)value;
+            writer.WriteStartObject();
+            writer.WritePropertyName(SerializedPayloadTag);
+            writer.WriteValue(env.SerializedPayload);
+            writer.WritePropertyName(PayloadContentTypeTag);
+            writer.WriteValue(env.PayloadContentType);
+            writer.WriteEndObject();
+        }
+    }
+}
---
+++ Solutions/Corvus.ContentHandling.Json/Microsoft/Extensions/DependencyInjection/ContentHandlingJsonServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="ContentHandlingJsonServiceCollectionExtensions.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    using Corvus.ContentHandling.Json.Internal;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// An installer for standard <see cref="JsonConverter"/>s.
+    /// </summary>
+    public static class ContentHandlingJsonServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add the default JSON serialization settings.
+        /// </summary>
+        /// <param name="services">The target service collection.</param>
+        /// <returns>The service collection.</returns>
+        public static IServiceCollection AddContentHandlingJsonConverters(this IServiceCollection services)
+        {
+            services.AddSingleton<JsonConverter, ContentTypeConverter>();
+            services.AddSingleton<JsonConverter, ContentEnvelopeConverter>();
+            return services;
+        }
+    }
+}
